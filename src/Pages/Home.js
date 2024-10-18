d1baf2f56f80e96c0afe8ee5b65d5bd90d23de2b
Monique de Souza <moniquejp16@gmail.com>
2024-10-18 12:33:03 -0300

    feat: logo and welcome added

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,11 @@
-
+import { All, ImageLogo, Welcome } from "../assets/Styles/HomeStyled";
+import Logo from "../assets/images/logo.png"
 
 export default function Home() {
 
-  return (<p>Monique</p>);
+  return (
+    <All>
+      <Welcome>Bem-vindo!</Welcome>
+      <ImageLogo src={Logo} alt="Logo da Txai" />
+    </ All>);
 }
---
+++ src/assets/Styles/HomeStyled.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const All = styled.div`
+    width: 100%;
+    height: 100vh;
+    background-color: #2f7171;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+
+export const ImageLogo = styled.img`
+    width: auto; 
+    height: auto; 
+    max-width: 100%; 
+    display: block;
+    margin-bottom: 20%;
+`
+export const Welcome = styled.p`
+    margin-right: 35%;
+    align-self: flex-end; 
+    font-size: 30px;
+    font-family: 'Arial', sans-serif;
+    color: #F8F8F8;
+    -webkit-text-stroke: 1px black; 
+    font-weight: 1000;
+    text-shadow: 2px 2px 5px rgba(0, 0, 0, 0.5);
+`
